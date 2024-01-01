1c2c29bf212615bdcbe44699b964236a1f11586a
wesley bruno <wesleybruno.adm@gmail.com>
2024-01-01 01:28:28 -0300

    Refatoração de codigo para deixar o arquivo cliente mais limpo e encapsular a regra de negocio da entidade animal

--- js/controllers/animalController.js
+++ js/controllers/animalController.js
@@ -1,42 +1,12 @@
-import { ImgurAPIService } from "../service/ImgurAPIService.js";
-import { RequestionBackendService } from "../service/RequestionBackendService.js";
+import { AnimalEntities } from "../entities/AnimalEntities.js";
 import { StorageService } from "../service/StorageService.js";
 // Caso os campos não estejam todos preenchidos ao clicar em salvar, deve aparecer um popup alertando o usuario de que deve completar os campos. Deve checar tambem se a foto está no localstorage.
 const button = document.getElementById("btn-salvar");
 button.onclick = (e) => {
     e.preventDefault();
 
-    const body = {
-        animal: {
-            photo: StorageService.get("photoAnimal") || "",
-            name: document.getElementById("nome").value || "",
-            city: document.getElementById("cidade").value || "",
-            state: document.getElementById("uf").value || "",
-            age: document.getElementById("idade").value || "",
-            measure: document.getElementById("medida").value || "",
-            size: document.getElementById("porte").value || "",
-            about: document.getElementById("sobre").value || "",
-            characteristics1:
-                document.getElementById("comportamento1").value || "",
-            characteristics2:
-                document.getElementById("comportamento2").value || "",
-        },
-        userId: "65909c4e6371fe4af617e855",
-    };
-
-    console.log(body);
-
-    RequestionBackendService.register("animals/register", body)
-        .then((animal) => {
-            window.location = "perfil.html";
-            console.log(animal);
-        })
-        .catch((err) => {
-            alert(
-                "Ocorreu algum erro no servidor. Tente novamente mais tarde ou contate nossa equipe técnica."
-            );
-            console.error(err.message);
-        });
+    const body = AnimalEntities.create();
+    AnimalEntities.register(body);
 };
 
 function fillnAllFields() {
@@ -51,30 +21,9 @@ function fillnAllFields() {
 }
 
 function handleAnimal(animalStorage) {
-    document
-        .getElementById("foto")
-        .setAttribute("src", `${animalStorage.photo}`);
-    document.getElementById("nome").value = animalStorage.name;
-    document.getElementById("telefone").value = animalStorage.telephone;
-    document.getElementById("cidade").value = animalStorage.city;
-    document.getElementById("uf").value = animalStorage.state;
-    document.getElementById("sobre").value = animalStorage.about;
+    AnimalEntities.fillForm(animalStorage);
 }
 
-const file = document.getElementById("file");
-file.onchange = () => {
-    const data = new FormData();
-    data.append("image", file.files[0]);
-
-    ImgurAPIService.save(data)
-        .then((res) => {
-            document
-                .getElementById("foto")
-                .setAttribute("src", `${res.data.link}`);
-
-            StorageService.set("photoAnimal", res.data.link);
-        })
-        .catch(console.error);
-};
+AnimalEntities.savePhoto();
 
 fillnAllFields();
---
+++ js/entities/AnimalEntities.js
@@ -0,0 +1,72 @@
+import { ImgurAPIService } from "../service/ImgurAPIService.js";
+import { RequestionBackendService } from "../service/RequestionBackendService.js";
+import { StorageService } from "../service/StorageService.js";
+
+export class AnimalEntities {
+    static register(body) {
+        RequestionBackendService.register("animals/register", body)
+            .then((animal) => {
+                window.location = "perfil.html";
+                console.log(animal);
+            })
+            .catch((err) => {
+                alert(
+                    "Ocorreu algum erro no servidor. Tente novamente mais tarde ou contate nossa equipe técnica."
+                );
+                console.error(err.message);
+            });
+    }
+
+    static uptade(body) {}
+
+    static delete(animalId) {}
+
+    static create() {
+        return {
+            animal: {
+                photo: StorageService.get("photoAnimal") || "",
+                name: document.getElementById("nome").value || "",
+                city: document.getElementById("cidade").value || "",
+                state: document.getElementById("uf").value || "",
+                age: document.getElementById("idade").value || "",
+                measure: document.getElementById("medida").value || "",
+                size: document.getElementById("porte").value || "",
+                about: document.getElementById("sobre").value || "",
+                characteristics1:
+                    document.getElementById("comportamento1").value || "",
+                characteristics2:
+                    document.getElementById("comportamento2").value || "",
+            },
+            userId: StorageService.get("userId"),
+        };
+    }
+
+    static fillForm(animalStorage) {
+        document
+            .getElementById("foto")
+            .setAttribute("src", `${animalStorage.photo}`);
+        document.getElementById("nome").value = animalStorage.name;
+        document.getElementById("telefone").value = animalStorage.telephone;
+        document.getElementById("cidade").value = animalStorage.city;
+        document.getElementById("uf").value = animalStorage.state;
+        document.getElementById("sobre").value = animalStorage.about;
+    }
+
+    static savePhoto() {
+        const file = document.getElementById("file");
+        file.onchange = () => {
+            const data = new FormData();
+            data.append("image", file.files[0]);
+
+            ImgurAPIService.save(data)
+                .then((res) => {
+                    document
+                        .getElementById("foto")
+                        .setAttribute("src", `${res.data.link}`);
+
+                    StorageService.set("photoAnimal", res.data.link);
+                })
+                .catch(console.error);
+        };
+    }
+}
