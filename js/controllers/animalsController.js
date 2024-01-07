c9791e9d9fe4e8e1f391803e690d393255d9fdd0
wesley bruno <wesleybruno.adm@gmail.com>
2024-01-07 17:28:00 -0300

    Alterei o modo que a pagina de animals realiza o filtro

--- js/controllers/animalsController.js
+++ js/controllers/animalsController.js
@@ -2,36 +2,12 @@ import { AnimalEntities } from "../entities/AnimalEntities.js";
 import { StorageService } from "../services/StorageService.js";
 import { AnimalView } from "../views/AnimalView.js";
 
-/**
- const cards = document.querySelectorAll("[data-card]");
- * Futuramente devo usar esse trecho de código para filtrar por cidade
- * 
-filtro.addEventListener("change", filtrar);
-function filtrar() {
-    let estadoSelecionado = filtro.options[filtro.selectedIndex].text;
-
-    if (estadoSelecionado == "Mostrar Todos")
-        cards.forEach((card) => card.classList.remove("ocultar_card"));
-    else {
-        cards.forEach((card) => {
-            let estado = card.querySelector("[data-estado]").textContent;
-
-            if (estadoSelecionado == estado)
-                card.classList.remove("ocultar_card");
-            else card.classList.add("ocultar_card");
-        });
-    }
-}
- */
-
 function callError(err) {
     console.error(err.message);
     alert(
         "Ocorreu algum erro ao carregar esta pagina. Tente novamente mais tarde ou contate nossa equipe técnica."
     );
 }
-
-const filtro = document.querySelector("[data-campoFiltro]");
 
 function showCards(state) {
     const listRef = document.getElementById("catalogo");
@@ -49,10 +25,12 @@ function showCards(state) {
 }
 
 function filtrar() {
-    let estadoSelecionado = filtro.options[filtro.selectedIndex].text;
+    let estadoSelecionado = filtro.value;
     if (estadoSelecionado == "Mostrar Todos") showCards("");
     else showCards(estadoSelecionado);
 }
 
+const filtro = document.querySelector("[data-campoFiltro]");
 filtro.addEventListener("change", filtrar);
+
 showCards(StorageService.get("user").state);
