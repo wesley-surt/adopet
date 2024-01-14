d21163394269aeb1882a2556f24a2a566ecc0e74
wesley bruno <wesleybruno.adm@gmail.com>
2024-01-14 19:00:27 -0300

    Tentando resolver o problema da index3

---
+++ src/js/controllers/indexController.js
@@ -0,0 +1,10 @@
+var botoes = document.querySelectorAll(".botao");
+botoes.forEach((botao) => {
+    botao.onclick = (evento) => {
+        if (evento.target.name === "login") {
+            window.location = "./src/html/login.html";
+        } else if (evento.target.name === "register") {
+            window.location = "./src/html/register.html";
+        }
+    };
+})
